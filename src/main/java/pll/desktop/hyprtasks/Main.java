5cedb7d8e797cedd60012547573c29ae4d0b3348
PLink <yagudinpavel1313@gmail.com>
2026-09-18 18:46:11 +0500

    Markup and controller

---
+++ src/main/java/pll/desktop/hyprtasks/Main.java
@@ -0,0 +1,24 @@
+package pll.desktop.hyprtasks;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/markup/main.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ src/main/java/pll/desktop/hyprtasks/MainController.java
@@ -0,0 +1,14 @@
+package pll.desktop.hyprtasks;
+
+import javafx.event.ActionEvent;
+
+public class MainController {
+
+    public void relocate(ActionEvent event) {
+    }
+
+
+    public void addItem(ActionEvent event) {
+    }
+
+}
